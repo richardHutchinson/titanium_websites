28b7a541a6bb76da0619bb5607aa95a37bc85d61
Richard Hutchinson <hutchinsonrichard1@gmail.com>
2013-10-05 10:21:31 -0400

    test

---
+++ test_sql/Resources/products/product_category.js
@@ -0,0 +1,40 @@
+// create var for the currentWindow
+var currentWin = Ti.UI.currentWindow;
+
+// set the data from the database to the array
+function setData() {
+	var db = Ti.Database.install('../sql/test','test');
+	var rows = db.execute('SELECT * FROM testCategory');
+	var dataArray = [];
+	
+	while (rows.isValidRow()) {
+	    dataArray.push({title:'' + rows.fieldByName('category') + '', hasChild:true, path:'../products/products.js'});
+	    
+	    rows.next();
+	};
+	
+	tableview.setData(dataArray);
+};
+
+// create table view
+var tableview = Ti.UI.createTableView({
+});
+
+tableview.addEventListener('click', function(e) {
+	if(e.rowData.path) {
+		var win = Ti.UI.createWindow({
+		    url:e.rowData.path,
+		    title:e.rowData.title
+		});
+		
+		var prodCat = e.rowData.title;
+		win.prodCat = prodCat;
+		Ti.UI.currentTab.open(win);
+    }
+});
+
+// add the tableView to the current window
+currentWin.add(tableview);
+
+// call the setData function to attach the database results to the array
+setData();
