6f3271b87c6ef2ef6f4fcf8efb6eb2784d26eebb
Richard Hutchinson <hutchinsonrichard1@gmail.com>
2013-10-05 16:16:33 -0400

    updates

---
+++ week2/Resources/home.js
@@ -0,0 +1,21 @@
+var games = {
+	"RTS": {
+		"title": "Real Time Strategy",
+		"footerTitle": "Strategy based games",
+		"gameList": [
+			{
+				"abcd": "ABCD",
+				"abcde": "ABCDE"
+			},
+			{
+				"abcdef": "ABCDEF",
+				"abcdefg": "ABCDEFG"
+			}
+		]
+	},
+	"aThroughI": [
+		{
+			"abcdefgh": "ABCDEFGH"
+		}
+	]
+};
