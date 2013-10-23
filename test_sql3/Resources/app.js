070585378011a8d95f5726f7f238433a73de270a
Richard Hutchinson <hutchinsonrichard1@gmail.com>
2013-10-23 14:18:49 -0400

    updates

---
+++ test_sql3/Resources/app.js
@@ -0,0 +1,31 @@
+var win = Ti.UI.createWindow({backgroundColor: "#222"});
+var listView = Ti.UI.createListView();
+var sections = [];
+
+var fruitSection = Ti.UI.createListSection({ headerTitle: 'Fruits'});
+var fruitDataSet = [
+    {properties: { title: 'Apple'}},
+    {properties: { title: 'Banana'}},
+];
+fruitSection.setItems(fruitDataSet);
+sections.push(fruitSection);
+
+var vegSection = Ti.UI.createListSection({ headerTitle: 'Vegetables'});
+var vegDataSet = [
+    {properties: { title: 'Carrots'}},
+    {properties: { title: 'Potatoes'}},
+];
+vegSection.setItems(vegDataSet);
+sections.push(vegSection);
+
+listView.sections = sections;
+win.add(listView);
+win.open();
+
+var fishSection = Ti.UI.createListSection({ headerTitle: 'Fish'});
+var fishDataSet = [
+    {properties: { title: 'Cod'}},
+    {properties: { title: 'Haddock'}},
+];
+fishSection.setItems(fishDataSet);
+listView.appendSection(fishSection);
---
+++ test_sql3/Resources/tabs/page_tabs.js
@@ -0,0 +1,7 @@
+Ti.UI.setBackgroundColor("#222");
+
+var win1 = Ti.UI.createWindow();
+
+Ti.include("tabs/page_tabs.js");
+
+win1.open();
