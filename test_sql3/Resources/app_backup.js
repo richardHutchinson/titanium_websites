f7c87550ec7a693aa0cbd1b2bfd3f31a1d3de373
Richard Hutchinson <hutchinsonrichard1@gmail.com>
2013-10-29 16:54:52 -0400

    updates

--- test_sql3/Resources/app_backup.js
+++ test_sql3/Resources/app_backup.js
@@ -1,63 +1,64 @@
-var win = Ti.UI.createWindow({
-	title: "List of Items",
-	backgroundColor: "#589"
-});
-
-var dataArray = [];
-
-var db = Ti.Database.install('/sql/test','test');
+var db = Ti.Database.install('/sql/test', 'test');
 var rows = db.execute('SELECT * FROM products');
 
-if(Ti.UI.iOS) {
-	var listView = Ti.UI.createListView({
-		backgroundColor: "#035",
-		top: 20
-	});
-}else {
-	var listView = Ti.UI.createListView({
-		backgroundColor: "#035"
-	});
-}
+var items = [];
 
-var dataSection = Ti.UI.createListSection();
-//console.log(rows.fieldByName("category"));
+while (rows.isValidRow()) {
 
-while(rows.isValidRow()) {
-	
 	//console.log(rows.fieldByName("category"));
-	
-	dataArray.push({
-		properties: {
-			title: "" + rows.fieldByName("category") + ""
-		}
-	});
 
-	/*var dataSet = [
-		{
-			properties: {
-				title: "" + rows.fieldByName("category") + "",
-				color: "#000",
-				font: {
-					fontSize: 20,
-					fontWeight: "bold"
-				},
-				backgroundColor: "#248"
-			}
+	/*items.push({
+	 properties: {
+	 title: "" + rows.fieldByName("category") + ""
+	 }
+	 });*/
+
+	var item = {
+		properties : {
+			title : "" + rows.fieldByName("category") + "",
+			color : "#000",
+			font : {
+				fontSize : 20,
+				fontWeight : "bold"
+			},
+			backgroundColor : "#248"
 		}
-	];*/
-	
-	console.log(dataArray);
-	
-	//console.log(dataSet);
-	rows.next(); //note: used for while loop
+	};
+
+	items.push(item);
+
+	//console.log(item);
+	rows.next();
 }
 
-console.log(dataArray);
+// -- model complete
 
-dataSection.setItems(dataArray);
-dataArray.push(dataSection);
+var win = Ti.UI.createWindow({
+	backgroundColor : "#589"
+});
 
-listView.sections = dataArray;
+if (Ti.UI.iOS) {
+	var listView = Ti.UI.createListView({
+		backgroundColor : "#035",
+		top : 20
+	});
+} else {
+	var listView = Ti.UI.createListView({
+		backgroundColor : "#035"
+	});
+}
+
+var sections = [];
+//note: returns an object
+var section = Ti.UI.createListSection();
+//note: connects the array to the object
+section.items = items;
+//note: pushes the array object into the array - ex: [{[]}]
+sections.push(section);
+
+//console.log(sections);
+
+listView.sections = sections;
 win.add(listView);
 win.open();
 
